ef32d0dd9e5bda445d26678b4aa2f76c53fd9c0e
Ali Kumbul <ali.kumbul@iotiq.net>
2024-02-01 16:51:24 +0300

    login and register company handling

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,9 +1,8 @@
 import _ from "lodash";
 import dataSource from "../config/datasource.config";
-import { Company } from "../entities/company.entity";
-import { User } from "../entities/user.entity";
-import * as bcrypt from "bcrypt";
 import jwtHelper from "../helpers/jwt.helper";
+import { User } from "../entities/user.entity";
+import { Company } from "../entities/company.entity";
 import { Session } from "../entities/session.entity";
 import { JwtPayload } from "../interfaces/jwtPayload.interface";
 
@@ -12,35 +11,51 @@ const removeCredentials = (userObject: User) => {
 };
 
 const userRepo = dataSource.getRepository(User),
-  sessionRepo = dataSource.getRepository(Session);
+  sessionRepo = dataSource.getRepository(Session),
+  companyRepo = dataSource.getRepository(Company);
 
 const authService = {
-  async register(email: string, password: string, company: Company) {
+  async register(email: string, password: string, company: string) {
     const newUser = new User();
 
+    let foundCompany;
+    foundCompany = await companyRepo.findOne({ where: { name: company } });
+    if(!foundCompany){
+      foundCompany = new Company();
+      foundCompany.name = company;
+    }
     newUser.hashPassword(password);
     newUser.email = email;
-    newUser.company = company;
 
     try {
-      const savedUser = await userRepo.save(newUser);
+      let savedUser;
+      await dataSource.transaction(async transactionalEM => {
+        const savedCompany = await transactionalEM.save(foundCompany!);
+        newUser.company = <Company>_.omit(savedCompany, ['deletedAt', 'createdAt', 'updatedAt']);
+        savedUser = await transactionalEM.save(newUser);
+      })
+      
       _.set(
-        savedUser,
+        savedUser!,
         "authToken",
-        await jwtHelper.signJwt(savedUser.id, savedUser.email)
+        await jwtHelper.signJwt(savedUser!.id, savedUser!.email)
       );
-      return removeCredentials(savedUser);
-    } catch (err) {
-      throw err;
+      return removeCredentials(savedUser!);
+    } catch (err: any) {
+      throw err.message;
     }
   },
   async login(email: string, password: string) {
-    const user = await userRepo.findOne({ where: { email: email } });
-
+    const user = await userRepo.createQueryBuilder('user')
+    .leftJoin('user.company', 'company')
+    .select(['user', 'company.id', 'company.name'])
+    .where('user.email = :email', { email: email })
+    .getOne();
+    
     if (!user || user.deletedAt) {
       throw new Error("User not found.");
     }
-
+    
     if (!user.checkPassword(password)) {
       throw new Error("Invalid credentials.");
     }
@@ -59,8 +74,8 @@ const authService = {
 
     try {
       await sessionRepo.remove(session);
-    } catch (error) {
-      throw error;
+    } catch (err: any) {
+      throw err.message;
     }
   },
 };
