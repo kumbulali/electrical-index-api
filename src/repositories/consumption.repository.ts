a1d7b7b0750d92ed5d8c0fd48b540b59d9e233f6
Ali Kumbul <ali.kumbul@iotiq.net>
2024-02-01 19:35:22 +0300

    consumption ve index repositories

---
+++ src/repositories/consumption.repository.ts
@@ -0,0 +1,6 @@
+import dataSource from "../config/datasource.config";
+import { Consumption } from "../entities/consumption.entity";
+
+const consumptionRepo = dataSource.getRepository(Consumption);
+
+export default consumptionRepo;
---
+++ src/repositories/index.repository.ts
@@ -0,0 +1,6 @@
+import dataSource from "../config/datasource.config";
+import { Index } from "../entities/index.entity";
+
+const indexRepo = dataSource.getRepository(Index);
+
+export default indexRepo;
